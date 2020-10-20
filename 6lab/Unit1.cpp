fcea9c61a24c5e300fe0612faa4550cfec71db46
eugenos-programos <71227355+eugenos-programos@users.noreply.github.com>
2020-10-20 18:07:53 +0300

    Update Unit1.cpp

--- 6lab/Unit1.cpp
+++ 6lab/Unit1.cpp
@@ -10,13 +10,20 @@
 #pragma argsused
 void main()
 {
-double **a, *b,s;
+double **a,s;     
 int i,j,n,m,tmp;
-printf(" Input size N,M: ");
+
+
+
+printf(" Input size N,M: ");      
 scanf("%d%d",&n,&m);
-a = new double*[n,m];
-for(i=0;i<n;i++)a[i] = new double[n,m];
-b = new double[n];
+
+double *b = new double[n];          
+
+a = new double*[n];                  
+for(i=0;i<n;i++)      a[i] = new double[n];          
+
+
 puts("\n Input Massiv A:");
 for(i=0;i<n;i++)for(j = 0;j<m;j++)scanf("%lf",&a[i][j]);
  for(i=0;i<n;i++){
@@ -27,11 +34,17 @@ for(i=0;i<n;i++)for(j = 0;j<m;j++)scanf("%lf",&a[i][j]);
  if(tmp == m-1)b[i] = 0;
  else b[i] = 1;
  }
-puts("Massiv B:");
+
+
+puts("Massiv B:");                               
 for(i = 0;i<n;i++)printf(" %3.0lf",b[i]);
+
+
+
+for(i = 0;i<n;i++) delete []a[i];
 delete []a;
 delete []b;
-puts("\nDelete...\nPress any key...");
+puts("\nDelete...\nPress any key...");            
 getch();
 }
 //---------------------------------------------------------------------------

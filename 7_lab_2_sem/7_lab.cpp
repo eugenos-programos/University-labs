9f7bc32ac3bc13d663c39e04ac7c410b1673cab6
eugenos-programos <71227355+eugenos-programos@users.noreply.github.com>
2021-03-24 22:26:03 +0300

    main

--- 7_lab_2_sem/7_lab.cpp
+++ 7_lab_2_sem/7_lab.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-/*   Rазработать проект для работы с деревом поиска, содержащий следующие обработчики, которые должны:
+/*Разработать проект для работы с деревом поиска, содержащий следующие обработчики, которые должны:
 – ввести информацию, состоящую из целочисленного ключа и строки текста;
 – записать информацию в дерево поиска;
 – сбалансировать дерево поиска;
@@ -20,15 +20,54 @@ public:
 }*root;
 
 
-void balance_tree() {
-
+Tree* create_root(int data, string name) {
+    Tree* new_node = new Tree;
+    new_node->data = data;
+    new_node->name = name;
+    new_node->left = new_node->right = NULL;
+    return new_node;
 }
 
 
-Tree* add_node(int data, string name, Tree* root)
-{
-    Tree* t = root;
+void view_tree(Tree* root,int level) {
+    string str;
+    if (root) {
+        view_tree(root->right, level + 1);		// Правое поддерево
+        for (int i = 0; i < level; i++) str = str + "    ";
+        cout << str << root->data;
+        view_tree(root->left, level + 1); 			// Левое поддерево
+    }
+}
 
+
+void create_list(Tree* root,int key, string name) {
+    if (root == NULL) { cout << "Empty tree\n"; return; };
+    Tree* prev, * t;
+    bool find = true;
+    t = root;
+    prev = t;
+    while ((t != NULL) && find) {
+        prev = t;
+        if (t->data == key) {
+            find = false;
+            cout << "Key has been used! Please try again!\n";
+        }
+        else {
+            if (key < t->data)t = t->left;
+            else t = t->right;   //перемещаемся по дереву по свойствам дерева поиска
+        }
+    }
+    if (prev == NULL)return;
+    if (find) {
+        t = create_root(key, name);
+        if (key > prev->data)prev->right = t;
+        else prev->left = t;
+    }
+}
+
+
+
+void balance_tree() {
 
 }
 
@@ -38,17 +77,32 @@ void find_info(int key, Tree* root) {
 
 
 
-Tree* create_tree(int data, string name) {
-    Tree* new_node = new Tree;
-    new_node->data = data;
-    new_node->name = name;
-    new_node->left = new_node->right = NULL;
-    return new_node;
-}
-
-
-
 int main()
 {
-    cout << "1-"
+    cout << "Creating a tree,input data and string:\n ";
+    int data;
+    string name;
+    cin >> data;
+    cin >> name;
+    root = create_root(data, name);
+    while (1) {
+        cout << "\n1 - Add node,2 - view,0 - eXIT\n";
+        int tmp;
+        cin >> tmp;
+        if (tmp == 1) {
+            cout << "Input data and string\n";
+            cin >> data;
+            cin >> name;
+            create_list(root, data, name);
+        }
+        if (tmp == 2) {
+            cout << "Tree:\n";
+            view_tree(root, 0);
+        }
+        if (tmp == 0) {
+            break;
+        }
+    }
+
+
 }

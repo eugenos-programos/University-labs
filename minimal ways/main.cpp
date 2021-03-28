22a8e2e8f6a9293b165c32da267bfcd1bac30796
eugenos-programos <71227355+eugenos-programos@users.noreply.github.com>
2021-03-28 14:41:33 +0300

    main

--- minimal ways/main.cpp
+++ minimal ways/main.cpp
@@ -1,96 +1,90 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <stdio.h>
 #include <cstring>
-
+#define max 1000000000
 using namespace std;
 
-/*Задан неориентированный взвешенный связный граф. Для каждой его вершины u выведите расстояние от первой вершины до u.
-
-Формат ввода:
-	Первая строка входного файла содержит числа n и m - количество вершин и ребер в
-графе соответственно (1 <= n <= 30 000, 0 <= m <= 400 000). Следующие m строк содержат по
-три числа - вершины, которые соединяет соответствующее ребро графа и его вес. Веса
-ребер неотрицательны и не превышают 10^4 .
-
-Формат вывода:
-	Выведите в выходной файл n чисел - для каждой вершины выведите расстояние от
-первой вершины до нее.
-
-Пример ввода:
-4 5
-1 2 1
-1 3 5
-2 4 8
-3 4 1
-2 3 3
-
-Пример вывода:
-0 1 4 5
+/*30000  399868
+* 30000  199955
+* 30000  29999
+* 979  271592
 */
 
-int main() {
-	ifstream file_for_reading("C:\\Tests\\path.in.txt");
-	int number_of_vertexes,edge;
-	if (!file_for_reading.is_open()) cout << "Open error\n";
-	else {
-			file_for_reading >> number_of_vertexes >> edge;
-	}
-	if (number_of_vertexes < 1 || number_of_vertexes >30000 || edge<0 || edge>400000) { cout << "Correct your vallues"; exit(1); };
-	int i;
-	int** matrix = new int* [number_of_vertexes];
-	for (i = 0; i < number_of_vertexes; i++)
-		matrix[i] = new int[number_of_vertexes];
-	for (i = 0; i < number_of_vertexes; i++) {
-		for (int j = 0; j < number_of_vertexes; j++) {
-			matrix[i][j] = 0;
+unsigned int minimumDist(unsigned int dist[], bool Tset[],unsigned int number)
+{
+	unsigned int min = INT_MAX, index;
+
+	for (unsigned int i = 0; i < number; i++)
+	{
+		if (Tset[i] == false && dist[i] <= min)
+		{
+			min = dist[i];
+			index = i;
 		}
 	}
+	return index;
+}
+
+void Dijkstra(short** matrix, unsigned int src,unsigned int number)
+{
+	unsigned int* dist = new unsigned int [number];                           
+	bool* Tset = new bool [number];
+
+
+	for (unsigned int i = 0; i < number; i++)
+	{
+		dist[i] = INT_MAX;
+		Tset[i] = false;
+	}
+
+	dist[src] = 0;      
+
+	for (unsigned int i = 0; i < number; i++)
+	{
+		unsigned int m = minimumDist(dist, Tset,number); // vertex not yet included.
+		Tset[m] = true;// m with minimum distance included in Tset.
+		for (unsigned int i = 0; i < number; i++)
+		{
+			// Updating the minimum distance for the particular node.
+			if (!Tset[i] && matrix[m][i] && dist[m] != INT_MAX && dist[m] + matrix[m][i] < dist[i])
+				dist[i] = dist[m] + matrix[m][i];
+		}
+	}
+	for (unsigned int i = 0; i < number; i++) {
+		free(matrix[i]);
+	}
+	free(matrix);
+	ofstream file_for_writing("C:\\Tests\\path.out");
+	for (unsigned int i = 0; i < number; i++) {
+		file_for_writing << dist[i] << " ";
+	}
+	file_for_writing.close(); 
+}
+
+
+unsigned int main() 
+{
+	ifstream file_for_reading("C:\\Tests\\path.in");
+	short number_of_vertexes;
+	int edge;
+	file_for_reading >> number_of_vertexes >> edge;
+	if (number_of_vertexes < 1 || number_of_vertexes > 30000 || edge < 0 || edge>400000) { cout << "Correct your vallues"; exit(1); };
+	short i;
+	short** matrix = new short* [number_of_vertexes];
+	for (i = 0; i < number_of_vertexes; i++)
+		matrix[i] = new short[number_of_vertexes];
+	unsigned int first_vertex, second_vertex, edge_size;
 	int tmp = 0;
 	while (tmp != edge) {
-		int first_vertex,second_vertex,edge_size;
+		int first_vertex, second_vertex, edge_size;
 		file_for_reading >> first_vertex >> second_vertex >> edge_size;
-		matrix[first_vertex-1][second_vertex-1] = matrix[second_vertex-1][first_vertex-1] = edge_size;
+		matrix[first_vertex - 1][second_vertex - 1] = matrix[second_vertex - 1][first_vertex - 1] = edge_size;
 		tmp++;
 	}
 	file_for_reading.close();
-	for (i = 0; i < number_of_vertexes; i++) {
-		for (int j = 0; j < number_of_vertexes; j++) {
-			cout << matrix[i][j] << " ";
-		}
-		cout << endl;
-	}
-	int* result_array = new int[number_of_vertexes];
-	int* marked_vertexes = new int[number_of_vertexes];
-	for (int i = 0; i < number_of_vertexes; i++) {
-		result_array[i] = 400001;                                            
-		marked_vertexes[i] = 1;
-	}
-	int begin_index,minimal_distance,minimal_index,temp_distance;
-	result_array[0] = 0;
-	do {
-		minimal_distance = minimal_index = 400001;
-		for ( i = 0; i < number_of_vertexes; i++) {
-			if ((marked_vertexes[i] == 1) && (result_array[i] < minimal_distance)) {
-				minimal_distance = result_array[i];
-				minimal_index = i; 
-			}
-		}
-		if (minimal_index != 400001) {
-			for ( i = 0; i < number_of_vertexes; i++) {
-				if (matrix[minimal_index][i] > 0) {
-					temp_distance = minimal_distance + matrix[minimal_index][i];
-					if (temp_distance < result_array[i]) result_array[i] = temp_distance;
-				}
-			}
-			marked_vertexes[minimal_index] = 0;
-		}
-	} while (minimal_index < 400001);
-	delete[] marked_vertexes;
-	for (i = 0; i < number_of_vertexes; i++)cout << result_array[i] << " ";
-	ofstream file_for_writing("C:\\Tests\\path.out.txt");
-	for (i = 0; i < number_of_vertexes; i++)file_for_writing << result_array[i] << " ";
-	file_for_writing.close();
+	Dijkstra(matrix, 0, number_of_vertexes);
 }

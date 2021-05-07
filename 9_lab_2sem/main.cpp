0f68d800c792e6cc72f6757e511e2e9ecfe5b58c
eugenos-programos <71227355+eugenos-programos@users.noreply.github.com>
2021-05-07 10:44:02 +0300

    roots

--- 9_lab_2sem/main.cpp
+++ 9_lab_2sem/main.cpp
@@ -1,58 +1,41 @@
-#include <SFML/Graphics.hpp>
-#include "Game.h"
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/*РќР°РїРёСЃР°С‚СЊ Рё РѕС‚Р»Р°РґРёС‚СЊ РїСЂРѕРіСЂР°РјРјСѓ РїРѕРёСЃРєР° РІСЃРµС…
+РєРѕСЂРЅРµР№ С„СѓРЅРєС†РёРё f(x) РЅР° РѕС‚СЂРµР·РєРµ [a, b] РІ СЃРѕРѕС‚РІРµС‚СЃС‚РІРёРё СЃ РІР°СЂРёР°РЅС‚РѕРј (С‚Р°Р±Р». 6.1).
+ РњРµС‚РѕРґ РЅР°С…РѕР¶РґРµРЅРёСЏ РєРѕСЂРЅСЏ РѕС„РѕСЂРјРёС‚СЊ РІ РІРёРґРµ РѕС‚РґРµР»СЊРЅРѕР№ С„СѓРЅРєС†РёРё, Р°Р»РіРѕСЂРёС‚Рј РєРѕС‚РѕСЂРѕР№ РѕРїРёСЃР°С‚СЊ Р±Р»РѕРє-СЃС…РµРјРѕР№. */
+
+ //x^2 - 10 * sin ^ 2(x) + 2  ... a = -1 ... b = 3 ... РњРµС‚РѕРґ РќСЊСЋС‚РѕРЅР°
+/*xв‰€-2.17806
+xв‰€-0.493435
+xв‰€0.493435
+xв‰€2.17806*/
+
+
+double proizv_func(double x){
+    double y;
+    y = 2*x - 20*sin(x)*cos(x);
+    return y;
+}
+
+ double func(double x){
+     double y;
+     y = x*x - 10*(sin(x))*(sin(x)) + 2;
+     return y;
+ }
+
+ double find_(double x,double eps){
+     do{
+        x = x - (func(x) / proizv_func(x));
+     }while(fabs(func(x)) > eps);
+     cout << x << endl;
+ }
 
 int main()
 {
-	// Создаем окно размером 600 на 600 и частотой обновления 60 кадров в секунду
-	sf::RenderWindow window(sf::VideoMode(600, 600), "15");
-	window.setFramerateLimit(60);
-
-	sf::Font font;
-	font.loadFromFile("Calibri.ttf");
-
-	// Текст с обозначением клавиш
-	sf::Text text("F2 - New Game / Esc - Exit / Arrow Keys - Move Tile", font, 20);
-	text.setFillColor(sf::Color::Cyan);
-	text.setPosition(5.f, 5.f);
-
-	// Создаем объект игры
-	Game game;
-	game.setPosition(50.f, 50.f);
-
-	sf::Event event;
-	int move_counter = 0;	// Счетчик случайных ходов для перемешивания головоломки
-
-	while (window.isOpen())
-	{
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed) window.close();
-			if (event.type == sf::Event::KeyPressed)
-			{
-				// Получаем нажатую клавишу - выполняем соответствующее действие
-				if (event.key.code == sf::Keyboard::Escape) window.close();
-				if (event.key.code == sf::Keyboard::Left) game.Move(Direction::Left);
-				if (event.key.code == sf::Keyboard::Right) game.Move(Direction::Right);
-				if (event.key.code == sf::Keyboard::Up) game.Move(Direction::Up);
-				if (event.key.code == sf::Keyboard::Down) game.Move(Direction::Down);
-				// Новая игра
-				if (event.key.code == sf::Keyboard::F2)
-				{
-					game.Init();
-					move_counter = 100;
-				}
-			}
-		}
-
-		// Если счетчик ходов больше нуля, продолжаем перемешивать головоломку
-		if (move_counter-- > 0) game.Move((Direction)(rand() % 4));
-
-		// Выполняем необходимые действия по отрисовке
-		window.clear();
-		window.draw(game);
-		window.draw(text);
-		window.display();
-	}
-
-	return 0;
+    find_(3,0.00001);
+    find_(0.7,0.00001);
+    find_(-0.1,0.00001);
+    system("\nPause");
 }
